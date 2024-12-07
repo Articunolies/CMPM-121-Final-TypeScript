beeee6c5f6f66950900691e3bcefb24202f32f99
Derek Simpson <articunolies22@gmail.com>
2024-12-07 00:10:04 -0800

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: 'CMPM-121-Final-TypeScript', 
+  base: '/CMPM-121-Final-TypeScript', 
   build: {
     outDir: 'dist',
   },
