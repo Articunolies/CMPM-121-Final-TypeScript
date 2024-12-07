c75d033559f92641a64c751b10b42fdd4a7542e6
Derek Simpson <articunolies22@gmail.com>
2024-12-07 00:25:06 -0800

    Vite update?

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/CMPM-121-Final-TypeScript/', 
+  base: '/CMPM-121-Final-TypeScript/', // Ensure this matches your repository name
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      },
+      output: {
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
   },
   resolve: {
     alias: {
